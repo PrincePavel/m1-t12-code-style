9bffc7b2803ddad945f52a4e2a8593a927c7a862
AleksandrLiagushin <a.lyagushin@yandex.ru>
2023-05-07 20:12:03 +0530

    Added some comments. Great job! =)

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -1,19 +1,30 @@
+/*
+в одной из статей Практикума читал что работа сдаваемая на проверку не должна иметь варнингов...
+точно не вспомню где. Вроде на Яндекс диске в требованиях к коду.
+Варианты исправления варнингов предложил в комментах внутри методов=)
+ */
+
 import java.util.Scanner;
 
 public class DepositCalculator {
+    /* Переменную yearRate желательно вынести как константу метода, т.к. при вызове метода передается невычисляемое
+    значение:
+    if (action == 1) {                                        здесь
+            finalAmount = calculateSimplePercent(depositAmount,0.06, depositPeriod);
+     */
     double calculateComplexPercent(double depositAmount, double yearRate, int depositPeriod) {
         double payoutSum = depositAmount * Math.pow((1 + yearRate / 12), 12 * depositPeriod);
         return roundNumbers(payoutSum,2);
-    }
+    } // в идеале методы должны быть разделены одной строкой
     double calculateSimplePercent(double depositAmount, double yearRate, int depositPeriod) {
         return roundNumbers(depositAmount + depositAmount * yearRate * depositPeriod,2);
-    }
-    double roundNumbers(double value, int places) {
-        double ScaLe = Math.pow(10, places);
+    } // в идеале методы должны быть разделены одной строкой
+    double roundNumbers(double value, int places) { // переменную places можно убрать - она не меняется
+        double ScaLe = Math.pow(10, places); // либо здесь меняем ее на 2, либо можно вынести константой класса
         return Math.round(value * ScaLe) / ScaLe;
     }
 
-    void runDepositCalculator() {
+    void runDepositCalculator() { // в этом методе все отлично поделено на группы! =)
         double finalAmount = 0;
         Scanner scanner = new Scanner(System.in);
 
